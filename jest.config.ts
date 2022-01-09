0c6ad417c3bd996379dcf3d0b954fe30890e7fee
Indra Kusuma <indrakusuma.udinus@gmail.com>
2022-01-09 13:26:58 +0700

    chore: adding test runner

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  verbose: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    'node_modules/variables/.+\\.(j|t)sx?$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['node_modules/(?!variables/.*)'],
+};
+
+export default config;
---
+++ src/__tests__/isDarkmode.spec.ts
@@ -0,0 +1,7 @@
+import isDarkmode from '../isDarkmode';
+
+describe('validating Invitato common helpers', () => {
+  it('should return true', () => {
+    expect(true).toBe(true);
+  });
+});
