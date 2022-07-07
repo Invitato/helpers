64025aaadb03288f2cea109186e34f236aae1495
Indra Kusuma <indrakusuma.udinus@gmail.com>
2022-07-07 20:59:45 +0700

    feat: adding vendors logo

---
+++ src/constants/vendors.ts
@@ -0,0 +1,64 @@
+const VENDOR_LOGO = {
+  dalang: {
+    name: 'Dalang',
+    logo: `https://user-images.githubusercontent.com/106049860/177788884-3a0eadcd-3041-4a5f-bc61-d0da2afa6592.png`,
+    whiteLogo: `https://user-images.githubusercontent.com/106049860/177788884-3a0eadcd-3041-4a5f-bc61-d0da2afa6592.png`,
+    igUrl: `https://www.instagram.com/dalangweddingorganizer/?hl=en`,
+  },
+  sadewa: {
+    name: 'Sadewa',
+    logo: `https://user-images.githubusercontent.com/106049860/177788908-4df31d3b-0264-438e-8047-e47a0bee9912.png`,
+    whiteLogo: `https://user-images.githubusercontent.com/106049860/177788908-4df31d3b-0264-438e-8047-e47a0bee9912.png`,
+    igUrl: `https://www.instagram.com/vian_sadewaweddingevent/?hl=en`,
+  },
+  flatter: {
+    name: `Flatter`,
+    logo: `https://user-images.githubusercontent.com/106049860/177788891-d89f1fce-d10e-4e31-bd12-91f7205e351e.png`,
+    whiteLogo: `https://user-images.githubusercontent.com/106049860/177788891-d89f1fce-d10e-4e31-bd12-91f7205e351e.png`,
+    igUrl: `https://www.instagram.com/flattereventrent/?hl=en`
+  },
+  pentone: {
+    name: `Pentone`,
+    logo: `https://user-images.githubusercontent.com/106049860/177788893-a3883cbd-ca13-4924-946f-4838c073fa5b.png`,
+    whiteLogo: `https://user-images.githubusercontent.com/106049860/177788896-d83e0e0f-d3f8-4398-9b0d-3eb51676904b.png`,
+    igUrl: `https://www.instagram.com/pentone.id/?hl=en`
+  },
+  prahasta: {
+    name: `Prahasta`,
+    logo: `https://user-images.githubusercontent.com/106049860/177788900-a26ca4af-148c-4e17-83e2-ef39087bba1d.png `,
+    whiteLogo: `https://user-images.githubusercontent.com/106049860/177788902-aa248cf1-e435-480a-9743-a0872da7ee9e.png`,
+    igUrl: `https://www.instagram.com/prahastaorganizer/?hl=en`
+  },
+  dreamworks: {
+    name: `Dreamworks`,
+    logo: `https://user-images.githubusercontent.com/106049860/177788888-9a4d0b13-c12c-4a86-b446-a6a78dc1bc67.png`,
+    whiteLogo: `https://user-images.githubusercontent.com/106049860/177788889-1d0b9e74-3a45-4577-9683-b49f09814b36.png`,
+    igUrl: `https://www.instagram.com/dreamworks_eo/?hl=en`
+  },
+  ashoka: {
+    name: `Ashoka`,
+    logo: `https://user-images.githubusercontent.com/106049860/177788880-6aad75b7-b61a-458e-ace1-d6456bcbc064.jpg`,
+    whiteLogo: `https://user-images.githubusercontent.com/106049860/177788880-6aad75b7-b61a-458e-ace1-d6456bcbc064.jpg`,
+    igUrl: `https://www.instagram.com/ashoka_wo/?hl=en`
+  },
+  samawa: {
+    name: `Samawa`,
+    logo: `https://user-images.githubusercontent.com/106049860/177788914-91d95f8c-2636-49fb-8eb6-c98210694f53.png`,
+    whiteLogo: `https://user-images.githubusercontent.com/106049860/177788914-91d95f8c-2636-49fb-8eb6-c98210694f53.png`,
+    igUrl: `https://www.instagram.com/samawawo/?hl=en`
+  },
+  tirto_wening: {
+    name: `Tirto Wening`,
+    logo: `https://user-images.githubusercontent.com/106049860/177788917-f22f5ef5-1a00-4161-8171-194ee5f72e83.png`,
+    whiteLogo: `https://user-images.githubusercontent.com/106049860/177788917-f22f5ef5-1a00-4161-8171-194ee5f72e83.png`,
+    igUrl: `https://www.instagram.com/tirto_wening_wo/?hl=en`
+  },
+  aksara: {
+    name: `Aksara Organizer`,
+    logo: `https://user-images.githubusercontent.com/106049860/177788852-7fd6d925-ef01-4e5d-9378-2e4b49ea4092.png`,
+    whiteLogo: `https://user-images.githubusercontent.com/106049860/177788874-72faa5d8-5ecc-4ba0-be61-3a4c820d1a06.png`,
+    igUrl: `https://www.instagram.com/aksara.organizer/?hl=en`
+  },
+}
+
+export default VENDOR_LOGO;
